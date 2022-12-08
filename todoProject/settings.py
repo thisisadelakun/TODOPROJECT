7cccafc6261545887d4b3bfad4c2945181a2c940
Adelakun <adelakunabayomi123@gmail.com>
2022-12-08 15:17:14 +0100

    second commit

--- todoProject/settings.py
+++ todoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d-j51--vls#b-kjjs!h49_xs@#!aain!%+&!yomd3dl0_izmqm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
